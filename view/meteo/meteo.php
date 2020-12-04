5af550e34961ddd4a448a1183b6c3750e5012674
LunaticCat <yannbrun1507@outlook.fr>
2020-12-04 02:47:19 +0100

    adding medium temp

--- view/meteo/meteo.php
+++ view/meteo/meteo.php
@@ -1,6 +1,6 @@
 <html>
 
-<p id="demo1"></p><img alt="temp" src="../../assets/temperature/cold.png" id='itemp'"/>
+<p id="demo1"></p><img alt="temp" src="../../assets/temperature/medium.png" id='itemp'"/>
 <p id="demo2"></p>
 
 <script>
@@ -28,10 +28,13 @@
             x.innerHTML = "Position : latitude= " + position.coords.latitude +"<br/> longitude=  " + position.coords.latitude;
             temp = meteo.current_condition.tmp;
             y.innerHTML = "Température actuelle:" + temp +"°C";
-            if(temp>=-273 && temp<25) {
+            if(temp>=-273 && temp<20) {
                 itemp.src = "../../assets/temperature/cold.png";
             }
-            else if(temp>=25 && temp<200){
+            else if(temp>=22 && temp<27){
+                itemp.src = "../../assets/temperature/medium.png";
+            }
+            else if(temp>=27 && temp<200){
                 itemp.src = "../../assets/temperature/hot.png";
             }
         }
